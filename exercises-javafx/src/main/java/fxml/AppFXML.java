371aa590148ace388eee67fa06178d173edcb3b0
Netto Pro <enzomnf_pro@outlook.com>
2025-04-23 12:35:49 -0300

    fxml configuration and study start

---
+++ exercises-javafx/src/main/java/fxml/AppFXML.java
@@ -0,0 +1,29 @@
+package fxml;
+
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AppFXML extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL FXMLfile = getClass().getResource("/fxml/Login.fxml");
+        GridPane root = FXMLLoader.load(FXMLfile);
+
+        Scene scene = new Scene(root, 350, 400);
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Login Screen");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- exercises-javafx/src/main/java/module-info.java
+++ exercises-javafx/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module javafx.ex {
     requires javafx.controls;
+    requires javafx.fxml;
 
     opens basic;
     opens layout;
+    opens fxml;
 }
